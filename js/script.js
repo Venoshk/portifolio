91c9a680446a91d30ef8e0eb2b5d9b988bb59dad
Henrique <venoshk.henrique19@gmail.com>
2024-06-09 16:54:27 -0300

    Adicionado about e modificando cards

--- js/script.js
+++ js/script.js
@@ -1,27 +1,24 @@
+const test = document.querySelector(".p");
+console.log(test)
 document.addEventListener("DOMContentLoaded", function() {
-  const chevron = document.querySelector("#scrollBtn");
-  const hamburger = document.querySelector(".hamburger");
-  const main = document.querySelector(".container_sidebar");
-  const body = document.querySelector("body");
-  
-  const scrollToNextSection = () => {
+const chevron = document.querySelector("#scrollBtn")
+
+
+
+const scrollToNextSection = () => {
       const nextSection = document.querySelector('section');
       if (nextSection) {
           nextSection.scrollIntoView({ behavior: 'smooth' });
       }
-  };
-
-  const toggleSidebar = () => {
-      if (checkbox.checked ) {
-          main.classList.add("open")
-        
-      } else {
-          main.classList.remove("open");
-         
-          
-      }
-  };
+  }
 
   chevron.addEventListener("click", () => scrollToNextSection());
-  hamburger.addEventListener('click', () => toggleSidebar());
 });
+
+
+
+
+
+
+
+
